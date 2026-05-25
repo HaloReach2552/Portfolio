15dbdb8066fe7975c8dc00d04afe974ddd56ba91
HaloReach2552 <73761580+HaloReach2552@users.noreply.github.com>
2026-05-25 18:31:21 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -135,6 +135,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
+    display: false,
     username: 'arifszn', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
