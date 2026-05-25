7acd67e5eb683faf9acaa38fd0a166e91fb7d56f
HaloReach2552 <73761580+HaloReach2552@users.noreply.github.com>
2026-05-24 21:15:25 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -120,16 +120,16 @@ const CONFIG = {
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Programa Especialización Data Scientist',
+      degree: 'Certificación',
+      from: '2016',
+      to: 'Actualidad',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'PUCP',
+      degree: 'Bachiller Sociología',
+      from: '2016',
+      to: '2023',
     },
   ],
   publications: [
